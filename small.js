82d07346cf20cdbc6cd63d33da692ce0816e39dd
baic <baic@miner.thehante.com>
2013-12-18 14:45:39 +0800

    update 2013.12.17

--- small.js
+++ small.js
@@ -420,6 +420,61 @@ typeof DEBUG === 'undefined' && (DEBUG = 1);
 			return array.length ? [].concat.apply([], array) : array;
 		}
 		DEBUG && console.timeEnd('dom');
+		DEBUG && console.time('env');
+        var platforms = [
+            // Android 2 - 4
+            {n: 'android', g: 0, r: /Android (\d+)/},
+            // iOS 3 - 7 / iPhone
+            {n: 'ios', g: 0, r: /iPhone OS (\d+)/, x:{iphone: 1}},
+            // iOS 3 - 7 / iPad
+            {n: 'ios', g: 0, r: /iPad;(?: U;)? CPU OS (\d+)/, x:{ipad: 1}},
+            // Windows Phone 7 - 8
+            {n: 'wpos', g: 0, r: /Windows Phone (?:OS )?(\d+)[.\d]+/},
+            // Kindle Fire
+            {n: 'android', g: 0, r: /Silk\/1./, v: 2, x:{silk: 1}},
+            // Kindle Fire HD
+            {n: 'android', g: 0, r: /Silk\/2./, v: 4, x:{silk: 2}},
+            // webOS 1 - 3
+            {n: 'webos', g: 0, r: /(?:web|hpw)OS\/(\d+)/},
+            // webOS 4 / OpenWebOS
+            {n: 'webos', g: 0, r: /WebAppManager|Isis/, v: 4},
+            // FirefoxOS
+            {n: 'ffos', g: 0, r: /Mobile;.*Firefox\/(\d+)/},
+            // Blackberry Playbook
+            {n: 'blackberry', g: 0, r: /PlayBook/i, v: 2},
+            // Blackberry 10+
+            {n: 'blackberry', g: 0, r: /BB1\d;.*Version\/(\d+\.\d+)/},
+            // Tizen
+            {n: 'tizen', g: 0, r: /Tizen (\d+)/},
+            // Safari
+            {n: 'safari', g: 1, r: /Version\/(\d+)[.\d]+.+Safari/},
+            // Chrome on iOS
+            {n: 'chrome', g: 1, r: /CriOS\/(\d+)[.\d]+.+Safari+/},
+            // Chrome
+            {n: 'chrome', g: 1, r: /Chrome\/(\d+)[.\d]+/},
+            // IE 8 - 10
+            {n: 'ie', g: 1, r: /MSIE (\d+)/},
+            // IE 11
+            {n: 'ie', g: 1, r: /Trident\/.*; rv:(\d+)/},
+            // desktop Firefox
+            {n: 'firefox', g: 1, r: /Firefox\/(\d+)/}
+        ];
+        var env = {
+            userAgent : WIN.navigator.userAgent,
+            msPointEnabled : !!WIN.navigator.userAgent,
+            browser : platforms.map(function(item){
+                var matche = item.r.exec(WIN.navigator.userAgent);
+                if(matche)return matche;
+            }),
+            screen : {
+                width : window.innerWidth,
+                height : window.innerHeight
+            }
+        };
+        small.extend({
+            env:env
+        });
+		DEBUG && console.timeEnd('env');
 		DEBUG && console.time('event');
         small.extend(small.fn,{
             listen : function(){},
