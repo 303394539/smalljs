38815bd7f14de4d0885fbbbdf539225c322a8910
baic <303394539@qq.com>
2014-05-06 11:05:45 +0800

    update code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,44 @@
 module.exports = function(grunt) {
-  var uglifyCompressOptions = {
-    global_defs: {
-      DEBUG: false,
-      DEVMODE: true,
+
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    meta: {
+      file: 'main',
+      banner: '/* <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy/m/d") %>\n' + '   <%= pkg.homepage %>\n' + '   Copyright (c) <%= grunt.template.today("yyyy/m/d") %> <%= pkg.author.name %> */\n'
     },
-    dead_code: true,
-    drop_console: true
-  };
-  var uglifyCompressOptionsProc = {
-    global_defs: {
-      DEBUG: false,
-      DEVMODE: false,
-    },
-    dead_code: true
-  };
 
-  var versions = ['1.0.0','2.0.0'];
-
-  var files = {};
-
-  versions.forEach(function(item){
-    files['small-'+item+'.min.js'] = 'small-'+item+'.js'
+    uglify: {
+      options: {
+        compress: {
+          global_defs: {
+            DEBUG: false,
+            DEVMODE: true,
+          },
+          dead_code: true,
+          drop_console: true,
+          hoist_funs: false,
+          loops: false,
+          unused: false
+        },
+        mangle: true,
+        preserveComments: false,
+        report: "min",
+        banner: "<%= meta.banner %>",
+        sourceMap: "small.min.map",
+        sourceMappingURL: "small.min.map",
+        beautify: {
+          ascii_only: true
+        }
+      },
+      build: {
+        files: {
+          'small.min.js': 'small-<%= pkg.version %>.js'
+        }
+      },
+    }
   });
-
-    grunt.initConfig({
-      pkg: grunt.file.readJSON("package.json"),
-      meta: {
-        file: 'main',
-        banner: '/* <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy/m/d") %>\n' + '   <%= pkg.homepage %>\n' + '   Copyright (c) <%= grunt.template.today("yyyy/m/d") %> <%= pkg.author.name %> */\n'
-      },
-
-      uglify: {
-        options: {
-          compress: uglifyCompressOptions,
-          mangle: true,
-          preserveComments: false,
-          report: "min",
-          banner: "<%= meta.banner %>",
-        },
-        build: {
-          files: files
-        },
-      },
-    });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
